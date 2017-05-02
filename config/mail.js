f7af8fe8d64e24d0395e07ff4699a56a718fee3f
moneynguyen0o0 <moneynguyen0o0@gmail.com>
2017-05-02 08:49:06 +0700

    Change config

--- config/mail.js
+++ config/mail.js
@@ -1,2 +1,2 @@
-export const USER = process.env.USER_EMAIL || 'trungnguyen1793@gmail.com';
-export const PASS = process.env.PASS_EMAIL || 'hoangtrung123'
+export const USER = process.env.USER_EMAIL;
+export const PASS = process.env.PASS_EMAIL;
